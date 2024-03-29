7619cd8e4d1b2e08034cb9ae067ecc7129489da4
Hamzzyy <chhamzarehman2135@gmail.com>
2024-03-29 15:20:21 +0500

    Minor updates

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,6 +2,6 @@
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2023 <a href="#">Pragmatic Software</a>.</strong> All rights
+    <strong>Copyright &copy; 2024 <a href="#">Hamza Rehman</a>.</strong> All rights
     reserved.
   </footer>
